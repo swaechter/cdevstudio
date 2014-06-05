0a1a2219b2675312584dac501b5ab085e134dd65
Simon Wächter <waechter.simon@gmail.com>
2014-06-05 08:34:16 +0200

    Added window menues

--- src/cdevstudioplatform/Window.cpp
+++ src/cdevstudioplatform/Window.cpp
@@ -2,6 +2,40 @@
 
 Window::Window() : QMainWindow()
 {
+	initWindow();
+	initMenues();
+}
+
+Window::~Window()
+{
+}
+
+QMenu *Window::getMenu(MenuTypes type)
+{
+	switch(type)
+	{
+		case Project:
+		{
+			return m_MenuProject;
+		}
+		case View:
+		{
+			return m_MenuView;
+		}
+		case Settings:
+		{
+			return m_MenuSettings;
+		}
+		case Help:
+		{
+			return m_MenuHelp;
+		}
+	}
+	return nullptr;
+}
+
+void Window::initWindow()
+{
 	QTabWidget *tabwidget = new QTabWidget(this);
 	setCentralWidget(tabwidget);
 	
@@ -9,6 +43,15 @@ Window::Window() : QMainWindow()
 	tabwidget->addTab(textedit, "Welcome");
 }
 
-Window::~Window()
+void Window::initMenues()
 {
+	m_MenuProject = new QMenu(tr("Project"), this);
+	m_MenuView = new QMenu(tr("View"), this);
+	m_MenuSettings = new QMenu(tr("Settings"), this);
+	m_MenuHelp = new QMenu(tr("Help"), this);
+	
+	menuBar()->addMenu(m_MenuProject);
+	menuBar()->addMenu(m_MenuView);
+	menuBar()->addMenu(m_MenuSettings);
+	menuBar()->addMenu(m_MenuHelp);
 }
--- src/cdevstudioplatform/Window.h
+++ src/cdevstudioplatform/Window.h
@@ -2,18 +2,39 @@
 #define WINDOW_H
 
 #include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QAction>
 #include <QtWidgets/QTableWidget>
 #include <QtWidgets/QTextEdit>
 
 #include "PlatformExport.h"
 
+enum MenuTypes
+{
+	Project,
+	View,
+	Settings,
+	Help
+};
+
 class CDEVSTUDIOPLATFORM_EXPORT Window : public QMainWindow
 {
-    Q_OBJECT
-    
+	Q_OBJECT
+	
 public:
-    Window();
-    virtual ~Window();
+	Window();
+	virtual ~Window();
+	QMenu *getMenu(MenuTypes type);
+	
+private:
+	QMenu *m_MenuProject;
+	QMenu *m_MenuView;
+	QMenu *m_MenuSettings;
+	QMenu *m_MenuHelp;
+	
+	void initWindow();
+	void initMenues();
 };
 
 #endif // WINDOW_H
