78f7f0f8b8713ddeb670a53e54ae4f89501cbc94
Simon Wächter <waechter.simon@gmail.com>
2014-06-05 12:22:38 +0200

    Added project manager

--- src/cdevstudioplatform/ProjectManager.cpp
+++ src/cdevstudioplatform/ProjectManager.cpp
@@ -1,11 +1,70 @@
 #include "ProjectManager.h"
 
-ProjectManager::ProjectManager(QObject *parent) : QObject(parent)
+ProjectManager::ProjectManager(QObject *parent): QObject(parent)
 {
-
+	m_Project = nullptr;
 }
 
 ProjectManager::~ProjectManager()
 {
+	closePossibleProject();
+}
 
+Project *ProjectManager::createProject(QString name, QString directory, QStringList files)
+{
+	if(!m_Project)
+	{
+		directory = directory.append(QString("/") + name + QString("/"));
+		Backend::createDirectory(directory);
+		Backend::createFile(directory + QString("Project.cdev"));
+		Backend::writeFile(directory + QString("Project.cdev"), name);
+		
+		setProject(new Project(name, directory));
+		
+		foreach(QString file, files)
+		{
+			QString filepath = directory + Backend::getNameOfFile(file);
+			Backend::createFile(filepath);
+			Backend::writeFile(filepath, Backend::readFile(file));
+		}
+	}
+	return m_Project;
+}
+
+Project *ProjectManager::loadProject(QString projectfile)
+{
+	if(!projectfile.isEmpty())
+	{
+		QString projectdirectory = Backend::getDirectoryOfFile(projectfile) + QString("/");
+		QString projectname = Backend::readFile(projectfile);
+		setProject(new Project(projectname, projectdirectory));
+	}
+	return m_Project;
+}
+
+Project *ProjectManager::getProject()
+{
+	return m_Project;
+}
+
+void ProjectManager::closeProject()
+{
+	closePossibleProject();
+}
+
+void ProjectManager::setProject(Project *project)
+{
+	closePossibleProject();
+	m_Project = project;
+	emit projectOpen();
+}
+
+void ProjectManager::closePossibleProject()
+{
+	if(m_Project)
+	{
+		emit projectClose();
+		delete m_Project;
+		m_Project = nullptr;
+	}
 }
--- src/cdevstudioplatform/ProjectManager.h
+++ src/cdevstudioplatform/ProjectManager.h
@@ -1,9 +1,11 @@
 #ifndef PROJECTMANAGER_H
 #define PROJECTMANAGER_H
 
-#include "PlatformExport.h"
-
 #include <QtCore/QObject>
+
+#include "PlatformExport.h"
+#include "Project.h"
+#include "Backend.h"
 
 class CDEVSTUDIOPLATFORM_EXPORT ProjectManager : public QObject
 {
@@ -12,6 +14,20 @@ class CDEVSTUDIOPLATFORM_EXPORT ProjectManager : public QObject
 public:
 	ProjectManager(QObject *parent);
 	~ProjectManager();
+	Project *createProject(QString name, QString directory, QStringList files);
+	Project  *loadProject(QString projectfile);
+	void closeProject();
+	Project *getProject();
+	
+private:
+	Project *m_Project;
+	
+	void setProject(Project *project);
+	void closePossibleProject();
+	
+signals:
+	void projectOpen();
+	void projectClose();
 };
 
 #endif // PROJECTMANAGER_H
